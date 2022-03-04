4ad69e8da60280d8ae50d37c988f2892ca875f7e
ilyaparad <prdxflv@mail.ru>
2022-03-04 20:06:48 +0400

    Initial Commit

---
+++ ios5-hw11-stepanov Ilya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios5-hw11-stepanov Ilya
+//
+//  Created by test on 04.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
